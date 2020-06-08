a9d0815fd7e0b02092ea4b7a1425e0f64925b4b3
shashank <shashank@Shashank-iMac.local>
2020-06-08 19:31:59 +0530

    initial changes

---
+++ shanlib/src/main/java/com/shan/shanlib/TestLib.java
@@ -0,0 +1,13 @@
+package com.shan.shanlib;
+
+import android.util.Log;
+
+public class TestLib {
+    TestLib(){
+
+    }
+
+    void printLogMessage(){
+        Log.d("TestLib","I m inside library");
+    }
+}
